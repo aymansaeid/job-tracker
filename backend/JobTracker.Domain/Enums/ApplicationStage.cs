616b823a0cea14fd9cb9e02878f5b6fb5bef3048
Ayman Saeid <aemon.saeid@gmail.com>
2026-03-19 21:28:41 +0300

    added my existing backend code

---
+++ backend/JobTracker.Domain/Enums/ApplicationStage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobTracker.Domain.Enums
+{
+    public enum ApplicationStage
+    {
+        Applied = 0,
+        InReview = 1,
+        Interview = 2,
+        Offer = 3,
+        Rejected = 4,
+        Ghosted = 5
+    }
+}
---
+++ backend/JobTracker.Domain/Enums/EmploymentType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobTracker.Domain.Enums
+{
+    public enum EmploymentType
+    {
+        Internship = 0,
+        FullTime = 1,
+        PartTime = 2,
+        Contract = 3
+    }
+}
